d5d3172bb54e56271810c53892a1c410284c762f
felipeandersonr <felipeatrodrigues34@gmail.com>
2025-03-06 02:21:46 -0300

    tdd in create/get transaction methods

---
+++ tests/transaction/test_get_transaction.py
@@ -0,0 +1,26 @@
+def test_get_transaction_success(client):
+    # buscar varias transacoes realizadas por um usuario com paginacao
+
+    pass
+
+
+def test_get_transactions_with_specific_user(client):
+    # buscar todas as transacoes relazidas com um unico usuario
+    
+    pass
+
+
+def test_get_incoming_transactions(client):
+    pass
+
+
+def test_get_outgoing_transactions(client):
+    pass
+
+
+def test_get_transactions_in_specific_datetime_range(client):
+    pass
+
+
+def test_get_transactions_with_invalid_pagination(client):
+    pass
