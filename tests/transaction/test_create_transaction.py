91f80e5d5ba6024bcd17b2e75db6b8457a71bea6
Felipe Anderson <59267574+felipeandersonr@users.noreply.github.com>
2025-03-20 12:08:54 -0300

    feat(api): added test when users are not friends (#10)

--- tests/transaction/test_create_transaction.py
+++ tests/transaction/test_create_transaction.py
@@ -3,14 +3,12 @@ from app.controller.transaction import TransactionController
 from app.models.user import User
 from app.utils.fake_data import get_random_nonexistent_id
 from tests.utils.friendship import create_test_friendship
-from tests.utils.user import create_test_user
 from tests.utils.wallet import create_test_wallet
 from tests.base_test_class import BaseTest
 
 
 class TestCreateTransaction(BaseTest):
-    def test_create_transaction_success(self, client, session, common_user_authenticated, wallet_from_common_user):       
-        another_user = create_test_user(session=session)
+    def test_create_transaction_success(self, client, session, common_user_authenticated, wallet_from_common_user, another_user):       
         wallet_from_another_user = create_test_wallet(session=session, user_id=another_user.id)
 
         wallet_from_common_user.balance = 1000
@@ -70,9 +68,7 @@ class TestCreateTransaction(BaseTest):
         assert response_json["error"] == "User has no wallet"
 
 
-    def test_create_transaction_when_user_has_no_wallet(self, client, session, common_user_authenticated):
-        another_user = create_test_user(session=session)
-
+    def test_create_transaction_when_user_has_no_wallet(self, client, session, common_user_authenticated, another_user):
         response = self.request_client_post(
             client=client,
             router_url="/transaction/create/",
@@ -90,8 +86,7 @@ class TestCreateTransaction(BaseTest):
         assert response_json["error"] == "User has no wallet"
 
 
-    def test_create_transaction_with_insufficient_balance_in_wallet(self, client, session, common_user_authenticated, wallet_from_common_user):
-        another_user = create_test_user(session=session)
+    def test_create_transaction_with_insufficient_balance_in_wallet(self, client, session, common_user_authenticated, wallet_from_common_user, another_user):
         wallet_from_another_user = create_test_wallet(session=session, user_id=another_user.id)
 
         wallet_from_common_user.balance = 10
@@ -132,3 +127,28 @@ class TestCreateTransaction(BaseTest):
         assert response_json["status"] == "Failed"
         assert response.status_code == HTTPStatus.BAD_REQUEST
         assert response_json["error"] == "Cannot carry out a transaction for you"
+
+
+    def test_create_transaction_when_users_are_not_friends(self, client, session, common_user_authenticated, another_user, wallet_from_common_user):
+        wallet_from_another_user = create_test_wallet(session=session, user_id=another_user.id)
+
+        wallet_from_common_user.balance = 1000
+
+        session.commit()
+        session.refresh(wallet_from_common_user)
+        
+        response = self.request_client_post(
+            client=client,
+            router_url="/transaction/create/",
+            authorization_token=common_user_authenticated.token,
+            json_data={
+                "value": 100, 
+                "destination_user_id": another_user.id
+            }
+        )
+
+        response_json = response.json()
+
+        assert response_json["status"] == "Failed"
+        assert response.status_code == HTTPStatus.FORBIDDEN
+        assert response_json["error"] == "Friendship required"
